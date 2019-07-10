5c24fb9fd9da5784f58b36699af558f300a00fc6
PANDA WU (ITA-ISDC-ISD-OOCLL/ZHA) <WUPA2@OOCL.COM>
2019-07-10 19:39:30 +0800

    finish prinReceipt

--- main/main.js
+++ main/main.js
@@ -1,6 +1,42 @@
 'use strict';
 
 //TODO: 请在该文件中实现练习要求并删除此注释
-function printReceipt(input){
-    
+function printReceipt(inputs) {
+    const countObject = countItem(inputs)
+    console.log(caculatePromotions(countObject));
+}
+function countItem(inputs) {
+    return inputs.reduce((acc, cur) => {
+        if (cur.includes('-')) {
+            let list = cur.split('-')
+            acc[list[0]] = acc[list[0]] + Number(list[1]) || Number(list[1])
+        }
+        else {
+            acc[cur] = ++acc[cur] || 1
+        }
+        return acc
+    }, {});
+}
+function caculatePromotions(countObject) {
+    const db = loadAllItems()
+    const promotions = loadPromotions()
+    let total = 0, discount = 0
+    let result = '***<没钱赚商店>收据***\n'
+    for (const key in countObject) {
+        db.forEach(item => {
+            if (key == item.barcode) {
+                if (countObject[key] >= 3 && promotions[0].barcodes.includes(key)) {
+                    discount += parseInt(countObject[key] / 3) * item.price
+                    total += countObject[key] * item.price - parseInt(countObject[key] / 3) * item.price
+                    result += `名称：${item.name}，数量：${countObject[key] + item.unit}，单价：${Number(item.price).toFixed(2)}(元)，小计：${(Number(countObject[key] - parseInt(countObject[key] / 3)) * item.price).toFixed(2)}(元)\n`
+                }
+                else {
+                    total += countObject[key] * item.price
+                    result += `名称：${item.name}，数量：${countObject[key] + item.unit}，单价：${Number(item.price).toFixed(2)}(元)，小计：${Number(countObject[key] * item.price).toFixed(2)}(元)\n`
+                }
+            }
+        })
+    }
+    result += `----------------------\n总计：${total.toFixed(2)}(元)\n节省：${discount.toFixed(2)}(元)\n**********************`
+    return result
 }
